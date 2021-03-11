4511dd72287757a396d6b3052e08b70febc04bf6
Breno Roosevelt <breno.jesus@ufms.br>
2021-03-11 16:53:14 -0400

    test IdDefinition

---
+++ tests/Definitions/Available/IdDefinitionTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Habemus\Test\Definitions\Available;
+
+use Habemus\Container;
+use Habemus\Definition\Available\IdDefinition;
+use Habemus\Exception\NotFound;
+use Habemus\Test\TestCase;
+
+class IdDefinitionTest extends TestCase
+{
+    public function testShouldCreateIdDefinition()
+    {
+        $definition = new IdDefinition('id1');
+        $this->assertEquals('id1', $definition->id());
+    }
+
+    public function testShouldGetIdDefinitionAsString()
+    {
+        $definition = new IdDefinition('id1');
+        $this->assertEquals('id1', (string) $definition);
+    }
+
+    public function testShouldResolveIdDefinitionWithContainer()
+    {
+        $container = new Container();
+        $container->add('id1', 10);
+        $definition = new IdDefinition('id1');
+        $this->assertEquals(10, $definition->getConcrete($container));
+    }
+
+    public function testShouldGetErrorIfIdDefinitionNotExists()
+    {
+        $definition = new IdDefinition('id1');
+        $this->expectException(NotFound::class);
+        $this->assertEquals(10, $definition->getConcrete(new Container()));
+    }
+}
